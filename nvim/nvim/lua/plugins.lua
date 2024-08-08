f396630d91300513becb840f5fed8a5f055efbaf
profz <visionxjarvis@gmail.com>
2024-08-08 13:18:29 +0530

    Replaced old nvim configuration with new setup

---
+++ nvim/nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+return {
+	-- other plugins
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = true,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+}
